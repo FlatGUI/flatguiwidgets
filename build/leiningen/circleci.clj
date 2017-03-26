84aa781f8be02cec198d90031f5e94c76afb2793
lebediev <qqqpppdmz05cbd>
2017-03-25 20:43:52 -0500

    deploy automation

--- build/leiningen/circleci.clj
+++ build/leiningen/circleci.clj
@@ -6,4 +6,5 @@
 (defn circleci [project & args]
   (let [branch (System/getenv "CIRCLE_BRANCH")]
     (condp re-find branch
-      #"master" (deploy/deploy project "clojars"))))
+      #"master" (deploy/deploy project "clojars")
+      nil)))
